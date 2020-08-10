24fe9c3d1ca466323d1294c71c601b3c0685a74a
MUROMACHIToshiki <ageki104@gmail.com>
2020-08-10 16:08:53 +0900

    ELAN解析結果をcsvとして保存

--- pympi_elan_parser.py
+++ pympi_elan_parser.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import csv
 import glob  # Import glob to easily loop over files
+import os
 import pympi  # Import pympi to work with elan files
 import string  # Import string to get the punctuation data
 
 
 def elan_read(file_path):
     """
-    ELANファイル読み込み後tab区切りで返す
-    :param file_path:
-    :return:
+    ELANファイルを解析後、二次元リストとして返す
+    :param file_path: ファイルパス
+    :return: ELAN解析結果
     """
     # elanファイル初期化
     eafob = pympi.Elan.Eaf(file_path)
     # 1ファイルの全発話
-    elan_data = [[]]
+    elan_data = []
 
     # 話者ごとにループ
     for ort_tier in ort_tier_names:
-        # 層がelanファイルに存在しない場合、エラーを発生させて続行
         if ort_tier not in eafob.get_tier_names():
             print('WARNING!!!')
             print('One of the ortography tiers is not present in the elan file')
             print('namely: {}. skipping this one...'.format(ort_tier))
-        # 層が存在する場合、アノテーションデータをループ
         else:
             for annotation in eafob.get_annotation_data_for_tier(ort_tier):
                 # 発話のみ取得
@@ -33,16 +32,31 @@ def elan_read(file_path):
                 utterance = annotation[2]
 
                 elan_data.append([start_time, end_time, utterance])
-                # print(start_time, end_time, ort_tier, utterance)
     return elan_data
 
 
+def elan_write(file_path, data):
+    """
+    ELANファイル解析結果をoutputディレクトリにcsvとして書き込み
+    :param file_path:解析するファイルパス
+    :param data:ELANファイル解析結果
+    :return:
+    """
+    output_path = output_root + os.path.splitext(os.path.basename(file_path))[0] + '.csv'
+    with open(output_path, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+
+
 if __name__ == '__main__':
-    corpus_root = '/home/toshiki/data/Chiba3Party/ELAN'
+    corpus_root = '/home/toshiki/data/Chiba3Party/ELAN/'
     output_root = '/home/toshiki/data/output/ELAN/'
     ort_tier_names = ['A.luu', 'B.luu', 'C.luu']  # 話者
 
     for f in glob.glob('{}/*.eaf'.format(corpus_root)):
+        file_data = elan_read(f)
+        elan_write(f, file_data)
+
         print("--*--*--*--")
         print(f)
-        print(elan_read(f))
+        print(file_data)
